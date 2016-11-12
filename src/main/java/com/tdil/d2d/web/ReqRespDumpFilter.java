3cd13ef64de7f0a94054f8dcdb1868f9a53da28f
mgodoy <mgodoy@despegar.com>
2016-11-12 16:52:10 -0300

    Agregado de log

---
+++ src/main/java/com/tdil/d2d/web/ReqRespDumpFilter.java
@@ -0,0 +1,445 @@
+package com.tdil.d2d.web;
+
+import java.io.BufferedReader;
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.PrintWriter;
+import java.util.Collection;
+import java.util.Enumeration;
+import java.util.HashMap;
+import java.util.Locale;
+import java.util.Map;
+
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ReadListener;
+import javax.servlet.ServletException;
+import javax.servlet.ServletInputStream;
+import javax.servlet.ServletOutputStream;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.WriteListener;
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletRequestWrapper;
+import javax.servlet.http.HttpServletResponse;
+
+import org.apache.commons.io.output.TeeOutputStream;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import com.tdil.d2d.utils.ServiceLocator;
+
+public class ReqRespDumpFilter implements Filter {
+
+	private static Logger logger = LogManager.getLogger(ReqRespDumpFilter.class);
+
+	@Override
+	public void init(FilterConfig filterConfig) throws ServletException {
+	}
+
+	@Override
+	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
+			throws IOException, ServletException {
+		if (ServiceLocator.isProd()) {
+			chain.doFilter(request, response);
+		} else {
+			try {
+				HttpServletRequest httpServletRequest = (HttpServletRequest) request;
+				HttpServletResponse httpServletResponse = (HttpServletResponse) response;
+
+				Map<String, String> requestMap = this.getTypesafeRequestMap(httpServletRequest);
+				BufferedRequestWrapper bufferedReqest = new BufferedRequestWrapper(httpServletRequest);
+				BufferedResponseWrapper bufferedResponse = new BufferedResponseWrapper(httpServletResponse);
+
+				final StringBuilder logMessage = new StringBuilder("REST Request - ").append("\n[HTTP URL:")
+						.append(httpServletRequest.getRequestURL().toString()).append("\n[HTTP METHOD:")
+						.append(httpServletRequest.getMethod()).append("] \n[PATH INFO:")
+						.append(httpServletRequest.getPathInfo()).append("] \n[REQUEST PARAMETERS:").append(requestMap)
+						.append("] \n[REQUEST BODY:").append(bufferedReqest.getRequestBody()).append("] \n[REMOTE ADDRESS:")
+						.append(httpServletRequest.getRemoteAddr()).append("]");
+
+				chain.doFilter(bufferedReqest, bufferedResponse);
+				logMessage.append(" \n[RESPONSE STATUS: ").append(bufferedResponse.getStatus()).append("] \n[RESPONSE BODY:").append(bufferedResponse.getContent()).append("]");
+				logger.warn(logMessage);
+			} catch (Throwable a) {
+				logger.error(a);
+			}
+		}
+	}
+
+	private Map<String, String> getTypesafeRequestMap(HttpServletRequest request) {
+		Map<String, String> typesafeRequestMap = new HashMap<String, String>();
+		Enumeration<?> requestParamNames = request.getParameterNames();
+		while (requestParamNames.hasMoreElements()) {
+			String requestParamName = (String) requestParamNames.nextElement();
+			String requestParamValue = request.getParameter(requestParamName);
+			typesafeRequestMap.put(requestParamName, requestParamValue);
+		}
+		return typesafeRequestMap;
+	}
+
+	@Override
+	public void destroy() {
+	}
+
+	private static final class BufferedRequestWrapper extends HttpServletRequestWrapper {
+
+		private ByteArrayInputStream bais = null;
+		private ByteArrayOutputStream baos = null;
+		private BufferedServletInputStream bsis = null;
+		private byte[] buffer = null;
+
+		public BufferedRequestWrapper(HttpServletRequest req) throws IOException {
+			super(req);
+			// Read InputStream and store its content in a buffer.
+			InputStream is = req.getInputStream();
+			this.baos = new ByteArrayOutputStream();
+			byte buf[] = new byte[1024];
+			int letti;
+			while ((letti = is.read(buf)) > 0) {
+				this.baos.write(buf, 0, letti);
+			}
+			this.buffer = this.baos.toByteArray();
+		}
+
+		@Override
+		public ServletInputStream getInputStream() {
+			this.bais = new ByteArrayInputStream(this.buffer);
+			this.bsis = new BufferedServletInputStream(this.bais);
+			return this.bsis;
+		}
+
+		String getRequestBody() throws IOException {
+			BufferedReader reader = new BufferedReader(new InputStreamReader(this.getInputStream()));
+			String line = null;
+			StringBuilder inputBuffer = new StringBuilder();
+			do {
+				line = reader.readLine();
+				if (null != line) {
+					inputBuffer.append(line.trim());
+				}
+			} while (line != null);
+			reader.close();
+			return inputBuffer.toString().trim();
+		}
+
+	}
+
+	private static final class BufferedServletInputStream extends ServletInputStream {
+
+		private ByteArrayInputStream bais;
+
+		public BufferedServletInputStream(ByteArrayInputStream bais) {
+			this.bais = bais;
+		}
+		
+		
+		
+		@Override
+		public void setReadListener(ReadListener readListener) {
+			// TODO Auto-generated method stub
+			
+		}
+
+		@Override
+		public int available() {
+			return this.bais.available();
+		}
+
+		@Override
+		public int read() {
+			return this.bais.read();
+		}
+
+		@Override
+		public int read(byte[] buf, int off, int len) {
+			return this.bais.read(buf, off, len);
+		}
+
+		@Override
+		public boolean isFinished() {
+			// TODO Auto-generated method stub
+			return false;
+		}
+
+		@Override
+		public boolean isReady() {
+			// TODO Auto-generated method stub
+			return false;
+		}
+
+	}
+
+	public class TeeServletOutputStream extends ServletOutputStream {
+
+		private final TeeOutputStream targetStream;
+
+		public TeeServletOutputStream(OutputStream one, OutputStream two) {
+			targetStream = new TeeOutputStream(one, two);
+		}
+
+		@Override
+		public void write(int arg0) throws IOException {
+			this.targetStream.write(arg0);
+		}
+
+		@Override
+		public void flush() throws IOException {
+			super.flush();
+			this.targetStream.flush();
+		}
+
+		@Override
+		public void close() throws IOException {
+			super.close();
+			this.targetStream.close();
+		}
+
+		@Override
+		public boolean isReady() {
+			// TODO Auto-generated method stub
+			return false;
+		}
+
+		@Override
+		public void setWriteListener(WriteListener writeListener) {
+			// TODO Auto-generated method stub
+			
+		}
+	}
+	
+
+	public class BufferedResponseWrapper implements HttpServletResponse {
+
+		HttpServletResponse original;
+		TeeServletOutputStream tee;
+		ByteArrayOutputStream bos;
+		PrintWriter other;
+		ByteArrayOutputStream otherByte;
+		int status;
+
+		public BufferedResponseWrapper(HttpServletResponse response) {
+			original = response;
+		}
+
+		public String getContent() {
+			if (bos != null) {
+				return bos.toString();
+			} else {
+				if (otherByte != null) {
+					return otherByte.toString();
+				} else {
+					return "";
+				}
+			}
+		}
+
+		@Override
+		public PrintWriter getWriter() throws IOException {
+			otherByte = new ByteArrayOutputStream();
+			other = new PrintWriter(otherByte);
+			return new TeePrintWriter(original.getWriter(),other);
+		}
+
+		@Override
+		public ServletOutputStream getOutputStream() throws IOException {
+			if (tee == null) {
+				bos = new ByteArrayOutputStream();
+				tee = new TeeServletOutputStream(original.getOutputStream(), bos);
+			}
+			return tee;
+
+		}
+
+		@Override
+		public String getCharacterEncoding() {
+			return original.getCharacterEncoding();
+		}
+
+		@Override
+		public String getContentType() {
+			return original.getContentType();
+		}
+
+		@Override
+		public void setCharacterEncoding(String charset) {
+			original.setCharacterEncoding(charset);
+		}
+
+		@Override
+		public void setContentLength(int len) {
+			original.setContentLength(len);
+		}
+
+		@Override
+		public void setContentType(String type) {
+			original.setContentType(type);
+		}
+
+		@Override
+		public void setBufferSize(int size) {
+			original.setBufferSize(size);
+		}
+
+		@Override
+		public int getBufferSize() {
+			return original.getBufferSize();
+		}
+
+		@Override
+		public void flushBuffer() throws IOException {
+			tee.flush();
+		}
+
+		@Override
+		public void resetBuffer() {
+			original.resetBuffer();
+		}
+
+		@Override
+		public boolean isCommitted() {
+			return original.isCommitted();
+		}
+
+		@Override
+		public void reset() {
+			original.reset();
+		}
+
+		@Override
+		public void setLocale(Locale loc) {
+			original.setLocale(loc);
+		}
+
+		@Override
+		public Locale getLocale() {
+			return original.getLocale();
+		}
+
+		@Override
+		public void addCookie(Cookie cookie) {
+			original.addCookie(cookie);
+		}
+
+		@Override
+		public boolean containsHeader(String name) {
+			return original.containsHeader(name);
+		}
+
+		@Override
+		public String encodeURL(String url) {
+			return original.encodeURL(url);
+		}
+
+		@Override
+		public String encodeRedirectURL(String url) {
+			return original.encodeRedirectURL(url);
+		}
+
+		@SuppressWarnings("deprecation")
+		@Override
+		public String encodeUrl(String url) {
+			return original.encodeUrl(url);
+		}
+
+		@SuppressWarnings("deprecation")
+		@Override
+		public String encodeRedirectUrl(String url) {
+			return original.encodeRedirectUrl(url);
+		}
+
+		@Override
+		public void sendError(int sc, String msg) throws IOException {
+			original.sendError(sc, msg);
+		}
+
+		@Override
+		public void sendError(int sc) throws IOException {
+			original.sendError(sc);
+		}
+
+		@Override
+		public void sendRedirect(String location) throws IOException {
+			original.sendRedirect(location);
+		}
+
+		@Override
+		public void setDateHeader(String name, long date) {
+			original.setDateHeader(name, date);
+		}
+
+		@Override
+		public void addDateHeader(String name, long date) {
+			original.addDateHeader(name, date);
+		}
+
+		@Override
+		public void setHeader(String name, String value) {
+			original.setHeader(name, value);
+		}
+
+		@Override
+		public void addHeader(String name, String value) {
+			original.addHeader(name, value);
+		}
+
+		@Override
+		public void setIntHeader(String name, int value) {
+			original.setIntHeader(name, value);
+		}
+
+		@Override
+		public void addIntHeader(String name, int value) {
+			original.addIntHeader(name, value);
+		}
+
+		@Override
+		public void setStatus(int sc) {
+			status = sc;
+			original.setStatus(sc);
+		}
+
+		@SuppressWarnings("deprecation")
+		@Override
+		public void setStatus(int sc, String sm) {
+			status = sc;
+			original.setStatus(sc, sm);
+		}
+		
+		public int getStatus() {
+			return status;
+		}
+
+		@Override
+		public void setContentLengthLong(long len) {
+			// TODO Auto-generated method stub
+			
+		}
+
+		@Override
+		public String getHeader(String name) {
+			// TODO Auto-generated method stub
+			return null;
+		}
+
+		@Override
+		public Collection<String> getHeaders(String name) {
+			// TODO Auto-generated method stub
+			return null;
+		}
+
+		@Override
+		public Collection<String> getHeaderNames() {
+			// TODO Auto-generated method stub
+			return null;
+		}
+
+	}
+
+}
