af8c010243aadec72d570ae941f17d24d41b95f6
Adriel Paredes <adriel.paredes@gmail.com>
2017-01-09 19:26:58 -0300

    DTD-70: Se agrega la entidad persisteible de nota

--- src/main/java/com/tdil/d2d/persistence/Note.java
+++ src/main/java/com/tdil/d2d/persistence/Note.java
@@ -1,10 +1,8 @@
 package com.tdil.d2d.persistence;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
-import javax.persistence.Table;
+import javax.persistence.*;
+import java.util.Date;
+import java.util.List;
 
 @Entity
 @Table(name = "D2D_NOTE")
@@ -15,6 +13,37 @@ public class Note implements PersistentEntity {
 	@Column(name = "id")
 	private long id;
 
+	@Column(name = "title")
+	private String title;
+
+	@Column(name = "subtitle", length = 512)
+	private String subtitle;
+
+	@Column(name = "content", length = 5120)
+	private String content;
+
+
+	@OneToMany
+	private List<Occupation> occupations;
+
+	@OneToMany
+	private List<Specialty> specialties;
+
+	@Column(name="category")
+	private NoteCategory category;
+
+	@Column(name = "is_active")
+	private boolean active;
+
+	@Column(name = "creation_date")
+	private Date creationDate;
+
+	@Column(name = "publishing_date")
+	private Date publishingDate;
+
+	@Column(name = "expiration_date")
+	private Date expirationDate;
+
 	public long getId() {
 		return id;
 	}
@@ -22,14 +51,85 @@ public class Note implements PersistentEntity {
 	public void setId(long id) {
 		this.id = id;
 	}
-	
-//	titulo
-//	subtitulo
-//	contenido
-//	
-//	n imagenes
-//	
-//	profesiones, especialidades
-//	url para redirigir
 
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+
+	public String getSubtitle() {
+		return subtitle;
+	}
+
+	public void setSubtitle(String subtitle) {
+		this.subtitle = subtitle;
+	}
+
+	public String getContent() {
+		return content;
+	}
+
+	public void setContent(String content) {
+		this.content = content;
+	}
+
+	public List<Occupation> getOccupations() {
+		return occupations;
+	}
+
+	public void setOccupations(List<Occupation> occupations) {
+		this.occupations = occupations;
+	}
+
+	public List<Specialty> getSpecialties() {
+		return specialties;
+	}
+
+	public void setSpecialties(List<Specialty> specialties) {
+		this.specialties = specialties;
+	}
+
+	public NoteCategory getCategory() {
+		return category;
+	}
+
+	public void setCategory(NoteCategory category) {
+		this.category = category;
+	}
+
+	public boolean isActive() {
+		return active;
+	}
+
+	public void setActive(boolean active) {
+		this.active = active;
+	}
+
+	public Date getCreationDate() {
+		return creationDate;
+	}
+
+	public void setCreationDate(Date creationDate) {
+		this.creationDate = creationDate;
+	}
+
+	public Date getPublishingDate() {
+		return publishingDate;
+	}
+
+	public void setPublishingDate(Date publishingDate) {
+		this.publishingDate = publishingDate;
+	}
+
+	public Date getExpirationDate() {
+		return expirationDate;
+	}
+
+	public void setExpirationDate(Date expirationDate) {
+		this.expirationDate = expirationDate;
+	}
 }
---
+++ src/main/java/com/tdil/d2d/persistence/NoteCategory.java
@@ -0,0 +1,22 @@
+package com.tdil.d2d.persistence;
+
+public enum NoteCategory {
+
+	CAT_1("Cursos, congresos y jornadas"),
+	CAT_2("Becas"),
+	CAT_3("Notas periodisticas"),
+	CAT_4("Promociones"),
+	CAT_5(" Productos y servcios de Doc to Doc y terceros asociados");
+
+
+	private String description;
+
+	private NoteCategory(String description) {
+		this.description = description;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+}
