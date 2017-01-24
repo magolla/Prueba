9c7799a65fe95f3edbd61e648eff4ae024473e6e
pablomendoza <thatdayinlondon@gmail.com>
2017-01-24 17:51:20 -0300

    correccion

--- src/main/java/com/tdil/d2d/exceptions/ExceptionDefinition.java
+++ src/main/java/com/tdil/d2d/exceptions/ExceptionDefinition.java
@@ -1,7 +1,6 @@
 package com.tdil.d2d.exceptions;
 
 public enum ExceptionDefinition {
-
 	// DTD_1xxx: Persistence Exceptions
 	DTD_1000("DTD_1000: Error trying to consume <<%s>> sponsor code"),
 
@@ -9,7 +8,8 @@ public enum ExceptionDefinition {
 	DTD_2001("DTD_2001: Error trying to retrieve logged in user"),
 	DTD_2002("DTD_2002: Error trying to get active subscriptions for user <<%s>>"),
 	DTD_2003("DTD_2003: Not subscription available for user <<%s>>"),
-	DTD_2004("DTD_2004: Error trying to create suscription");
+	DTD_2004("DTD_2004: Cannot add occupation to note"),
+	DTD_2005("DTD_2005: Cannot add speciality to note");
 
 	// DTD_3xxx: Controller Exceptions
 
