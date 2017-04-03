9839f163fd903af1749b62e7d7d663ecb449e16f
Cosmicgirl\pablo <facundo@tdil.com.ar>
2017-04-03 15:55:21 -0300

    Identado

--- src/main/java/com/tdil/d2d/dao/impl/NoteDAOImpl.java
+++ src/main/java/com/tdil/d2d/dao/impl/NoteDAOImpl.java
@@ -72,7 +72,7 @@ public class NoteDAOImpl extends HibernateDaoSupport implements NoteDAO {
 		queryString.append("WHERE (specialty.occupation.id IN (:ocuppations) ");
 		queryString.append("AND specialty.id IN (:specialities) ");
 		queryString.append("AND note.active = 1) ");
-		queryString.append("AND (specialty.id is null AND note.active = 1 )");
+		queryString.append("AND (specialty.id is null AND note.active = 1 ) ");
 		queryString.append("AND (specialty.occupation.id is null AND note.active = 1) ");
 		queryString.append("order by note.creationDate desc ");
 
