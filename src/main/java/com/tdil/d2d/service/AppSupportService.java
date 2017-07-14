68f3d72325916257826be2cf7fc23fbf77cdf0e3
Unknown <facundocetraro@hotmail.com>
2017-07-14 14:25:03 -0300

    Se crea el metodo que nos informa si hay que forzar el update de la app.

---
+++ src/main/java/com/tdil/d2d/service/AppSupportService.java
@@ -0,0 +1,7 @@
+package com.tdil.d2d.service;
+
+public interface AppSupportService {
+
+	boolean validateVersion(int version, int platform);
+
+}
---
+++ src/main/java/com/tdil/d2d/service/ValidationService.java
@@ -0,0 +1,33 @@
+package com.tdil.d2d.service;
+
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
+import com.tdil.d2d.exceptions.ServiceException;
+import com.tdil.d2d.persistence.Note;
+import com.tdil.d2d.persistence.Occupation;
+import com.tdil.d2d.persistence.Specialty;
+
+public interface ValidationService {
+
+	Note save(Note note);
+
+	List<Note> getNotes(int page, int size, Map<String, Object> params);
+
+	List<Note> getNotesForUser(int page, int size) throws ServiceException ;
+
+	Note getNoteById(Long id);
+
+	Set<Occupation> getOccupations(Long id);
+
+	Set<Specialty> getSpecialities(Long id);
+
+	Occupation addOccupation(Long id, Long occupationId);
+
+	Specialty addSpeciality(Long id, Long specialityId);
+
+	void disableNote(Long id);
+
+	Note getHomeNote()  throws ServiceException;
+}
