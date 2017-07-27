b920cd8dbc8746eac0534ac2207c3a582f08c499
pablomendoza <thatdayinlondon@gmail.com>
2017-07-27 14:38:47 -0300

    CONFLICTOS EN SUSCRIPTION

--- src/main/java/com/tdil/d2d/service/impl/SubscriptionServiceImpl.java
+++ src/main/java/com/tdil/d2d/service/impl/SubscriptionServiceImpl.java
@@ -1,6 +1,8 @@
 package com.tdil.d2d.service.impl;
 
 import java.util.Calendar;
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.Date;
 import java.util.List;
 
@@ -8,7 +10,13 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.tdil.d2d.controller.api.dto.InAppPurchaseDTO;
+import com.tdil.d2d.controller.api.dto.VerifyReceiptResultDTO;
+import com.tdil.d2d.controller.api.request.ReceiptSuscriptionRequest;
 import com.tdil.d2d.controller.api.request.RedeemSponsorCodeRequest;
+import com.tdil.d2d.controller.api.response.UserReceiptResponse;
 import com.tdil.d2d.dao.ActivityLogDAO;
 import com.tdil.d2d.dao.SubscriptionDAO;
 import com.tdil.d2d.dao.SystemPropertyDAO;
@@ -19,6 +27,7 @@ import com.tdil.d2d.exceptions.ExceptionDefinition;
 import com.tdil.d2d.exceptions.ServiceException;
 import com.tdil.d2d.persistence.ActivityAction;
 import com.tdil.d2d.persistence.ActivityLog;
+import com.tdil.d2d.persistence.Receipt;
 import com.tdil.d2d.persistence.Sponsor;
 import com.tdil.d2d.persistence.SponsorCode;
 import com.tdil.d2d.persistence.Subscription;
@@ -30,6 +39,10 @@ import com.tdil.d2d.service.SubscriptionService;
 import com.tdil.d2d.utils.Constants;
 import com.tdil.d2d.utils.LoggerManager;
 import com.tdil.d2d.utils.ServiceLocator;
+
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
 
 @Transactional
 @Service()
@@ -121,12 +134,13 @@ public class SubscriptionServiceImpl implements SubscriptionService {
 				// String.valueOf(userID));
 				return null;
 			} else {
-				Subscription subscription = subscriptions.get(0);
-				if (subscription.getExpirationDate().before(new Date())) {
-					return null;
-				} else {
-					return subscription;
+				for (Subscription subscriptionDB : subscriptions) {
+					if(subscriptionDB.getSponsorCode() != null) {
+						return subscriptionDB;
+					}
 				}
+				
+				return subscriptions.get(subscriptions.size()-1);
 			}
 		} catch (DAOException e) {
 			throw new DTDException(ExceptionDefinition.DTD_2002, e, String.valueOf(userID));
@@ -218,6 +232,138 @@ public class SubscriptionServiceImpl implements SubscriptionService {
 			throw new DTDException(ExceptionDefinition.DTD_2004, e);
 		}
 		
+		return null;
+	}
+	
+	@Override
+	public UserReceiptResponse verifyAndRegisterSuscription(User user, ReceiptSuscriptionRequest receiptSuscriptionRequest) throws ServiceException {
+		String url = "https://buy.itunes.apple.com/verifyReceipt";
+		if(receiptSuscriptionRequest.getIsDev()) {
+			url = "https://sandbox.itunes.apple.com/verifyReceipt";
+		}
+		
+        try {
+        	org.json.JSONObject data = new org.json.JSONObject();
+			data.put("receipt-data", receiptSuscriptionRequest.getReceiptData());
+			data.put("password", receiptSuscriptionRequest.getPassword());
+			
+        	okhttp3.MediaType jsonType = okhttp3.MediaType.parse("application/x-www-form-urlencoded");
+        	
+			OkHttpClient client = new OkHttpClient();
+			
+			okhttp3.RequestBody body = okhttp3.RequestBody.create(jsonType, data.toString());
+			Request request = new Request.Builder()
+			      .url(url)
+			      .post(body)
+			      .build();
+			Response response = client.newCall(request).execute();
+        	
+            String responseBody = response.body().string();
+            
+            Gson gson = new GsonBuilder().create();
+            VerifyReceiptResultDTO receiptResult = gson.fromJson(responseBody, VerifyReceiptResultDTO.class);
+            
+            InAppPurchaseDTO lastPurchase = null;
+            if(receiptResult.getStatus() == 0 && !receiptResult.getLatestPurchases().isEmpty()) {
+            	
+            	Comparator<InAppPurchaseDTO> comp = (InAppPurchaseDTO a, InAppPurchaseDTO b) -> {
+            		if (b.getExpiresDate() > a.getExpiresDate()){
+                        return -1;
+                    } else {
+                        return 1;
+                    }
+            	};
+            	
+            	List<InAppPurchaseDTO> latestPurchases = receiptResult.getLatestPurchases();
+            	
+            	Collections.sort(latestPurchases, comp);
+            	
+            	int quantity = latestPurchases.size();
+            	
+            	List<String> storedTransationIds = subscriptionDAO.getStoredTransationIds(latestPurchases);
+            	this.saveReceipts(latestPurchases, storedTransationIds);
+            	
+            	lastPurchase = latestPurchases.get(quantity - 1);
+            }
+            
+            if(lastPurchase != null) {
+            	Long expiresDate = lastPurchase.getExpiresDate();
+            	Long purchaseDate = lastPurchase.getPurchaseDate();
+            	Boolean isTrialPeriod = lastPurchase.getIsTrialPeriod();
+            	
+            	UserReceiptResponse receiptInfo = new UserReceiptResponse();
+            	receiptInfo.setExpiresDate(expiresDate);
+            	receiptInfo.setPurchaseDate(purchaseDate);
+            	receiptInfo.setIsTrialPeriod(isTrialPeriod);
+            	
+            	return receiptInfo;
+            }
+        } catch (Exception ex) {
+        	LoggerManager.error(this, ex);
+			throw new DTDException(ExceptionDefinition.DTD_2004, ex);
+        }
+        
+		return null;
+	}
+	
+	private void saveReceipts(List<InAppPurchaseDTO> latestPurchases, List<String> storedTransationIds) throws ServiceException {
+		try {
+			User loggedUser = this.getLoggedUser();
+			for (InAppPurchaseDTO inApp : latestPurchases) {
+				if(!storedTransationIds.contains(inApp.getTransactionID())) {
+					
+					Receipt newReceipt = new Receipt();
+					newReceipt.setUser(loggedUser);
+					newReceipt.setCreationDate(new Date());
+					newReceipt.setExpiresDate(inApp.getExpiresDate());
+					newReceipt.setIsTrialPeriod(inApp.getIsTrialPeriod());
+					newReceipt.setOrderItemId(inApp.getOrderItemID());
+					newReceipt.setTransationId(inApp.getTransactionID());
+					newReceipt.setTransactionIdOriginal(inApp.getTransactionIDOriginal());
+					newReceipt.setPurchaseDate(inApp.getPurchaseDate());
+					newReceipt.setPurchaseDateOriginal(inApp.getPurchaseDateOriginal());
+					newReceipt.setQuantity(inApp.getQuantity());
+					newReceipt.setProductId(inApp.getProductID());
+					
+		            subscriptionDAO.saveReceipt(newReceipt);
+				}
+			}
+			
+		} catch (DAOException e) {
+            throw new ServiceException(e);
+        }
+		
+	}
+	
+	private User getLoggedUser() throws ServiceException {
+		try {
+			return userDAO.getById(User.class, com.tdil.d2d.security.RuntimeContext.getCurrentUser().getId());
+		} catch (Exception e) {
+			throw new ServiceException(e);
+		}
+	}
+	
+	@Override
+	public UserReceiptResponse getLastReceipt(Long userId) throws ServiceException {
+        try {
+            Receipt lastReceipt = null;
+            lastReceipt = subscriptionDAO.getLastReceipt(userId);
+            
+            if(lastReceipt != null) {
+            	UserReceiptResponse receiptInfo = new UserReceiptResponse();
+            	receiptInfo.setExpiresDate(lastReceipt.getExpiresDate());
+            	receiptInfo.setPurchaseDate(lastReceipt.getPurchaseDate());
+            	receiptInfo.setIsTrialPeriod(lastReceipt.getIsTrialPeriod());
+            	
+            	return receiptInfo;	
+            }
+            
+            
+        } catch (Exception ex) {
+        	LoggerManager.error(this, ex);
+			throw new DTDException(ExceptionDefinition.DTD_2004, ex);
+        }
+        
 		return null;
 	}
 }
