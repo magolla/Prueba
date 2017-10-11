ffeb088ca36f285c4ba0fbacb6998a9bb2961088
Unknown <facundocetraro@hotmail.com>
2017-10-11 10:49:09 -0300

    * Se inicia el issue

--- src/main/java/com/tdil/d2d/persistence/Notification.java
+++ src/main/java/com/tdil/d2d/persistence/Notification.java
@@ -25,6 +25,9 @@ public class Notification implements PersistentEntity {
 	@ManyToOne
 	private JobOffer offer;
 	
+	@ManyToOne
+	private Note note;
+	
 	@Column(name = "action")
 	private String action;
 	
