e29b6ecc8a84518bf534f9ee1624bdb5fad593a6
facundo <facundo@tdil.com.ar>
2017-11-24 18:26:02 -0300

    * Se arregla una restriccion.

--- src/main/java/com/tdil/d2d/dao/impl/JobOfferDAOImpl.java
+++ src/main/java/com/tdil/d2d/dao/impl/JobOfferDAOImpl.java
@@ -662,7 +662,7 @@ public class JobOfferDAOImpl extends GenericDAO<JobOffer> implements JobOfferDAO
 	public List<JobOffer> getLastOffers() {
 		Criteria criteria = this.getSessionFactory().getCurrentSession().createCriteria(JobOffer.class);
 		criteria.addOrder(Order.desc("creationDate"));
-		criteria.add(Restrictions.le("offerDate", new Date()));
+		criteria.add(Restrictions.ge("offerDate", new Date()));
 		criteria.add(Restrictions.eq("status", JobOffer.VACANT));
 		criteria.setMaxResults(5);
 		List<JobOffer> list = criteria.list();
